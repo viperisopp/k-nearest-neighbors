9a25bc1dd1388f69258d5017486a1e1bb7c65bc8
viperisopp <michaely6828@gmail.com>
2025-07-27 21:00:03 -0700

    added code to knn

--- knn.py
+++ knn.py
@@ -1,4 +1,20 @@
 import numpy as np
 import pandas as pd
-import sklearn.model_selection
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing,neighbors
+
+df = pd.read_csv("datasets/iris_species.csv")
+
+y = df["Species"].to_numpy()
+
+x = df.drop(columns="Species").to_numpy()
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+classifier = neighbors.KNeighborsClassifier()
+classifier.fit(x_train,y_train)
+
+y_pred = classifier.predict(x_test)
+
+accuracy = classifier.score(x_test,y_pred)
+print(accuracy)
